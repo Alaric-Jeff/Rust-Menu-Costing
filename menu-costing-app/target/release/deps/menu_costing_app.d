2bbc9a22ab5a83f2b39f5d67cc9cdb3b20b8c8c2
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-08-03 11:55:15 +0800

    added a release exe.

---
+++ menu-costing-app/target/release/deps/menu_costing_app.d
@@ -0,0 +1,5 @@
+C:\Users\Public\Jeffrey Aspiras Projects\Console\menu-costing\Rust-Menu-Costing\menu-costing-app\target\release\deps\menu_costing_app.d: src\main.rs
+
+C:\Users\Public\Jeffrey Aspiras Projects\Console\menu-costing\Rust-Menu-Costing\menu-costing-app\target\release\deps\menu_costing_app.exe: src\main.rs
+
+src\main.rs:
